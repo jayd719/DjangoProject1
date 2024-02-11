eb9dd416eb0ec27fb4486b5ce7a7c230b4729456
JD-MBP <jashansingh29@gmail.com>
2024-02-10 23:32:30 -0500

    ;

--- blinker.py
+++ blinker.py
@@ -3,10 +3,11 @@ from gpiozero import LED
 from time import sleep
 
 l1= LED(17)
+l2= LED(22)
+l3= LED(27)
 
 
-
-LEDS =[l1]
+LEDS =[l1,l2,l3]
 
 
 c = True
@@ -15,10 +16,14 @@ while True:
     fh = open('ins.txt','r',encoding='utf-8')
     inst = fh.readline()
     if(inst=='on'):
-        LEDS[i].on()
+        LEDS[0].on()
+        LEDS[1].on()
+        LEDS[2].on()
        
     elif (inst =='off'):
-          LEDS[i].off()
+          LEDS[0].off()
+          LEDS[1].off()
+          LEDS[2].off()
         
     elif(inst=='blink'):
         if c:
